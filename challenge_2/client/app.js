5411981269acdc3f7ed0fa810d5a2b5bd6b35c24
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-04-21 13:22:46 -0700

    Create Basic HTML Form and Establish Server to Receive Post Requests

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,15 @@
+//********MODEL********
+
+
+//********VIEW*********
+
+//*****CONTROLLER******
+
+
+//this is wrong
+const onJSONSubmit = (data) => {
+  //calls a method in the model to post an http request
+  //attach to on submit button
+}
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const multer = require('multer');
+const upload = multer();
+
+const server = express();
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(upload.array());
 
 
+server.get('/', (req, res) => {
+
+});
+
+server.post('/' , (req, res) => {
+  console.log('Received a request from ' + req.hostname + '. Here it is: ' + JSON.stringify(req.body));
+  //res.render()
+
+  res.sendFile(path.join(__dirname + '/client/index.html'));
+
+});
+
+
+
+server.listen(3000, () => {
+   console.log('CSV report generator is listening on port 3000!');
+});
